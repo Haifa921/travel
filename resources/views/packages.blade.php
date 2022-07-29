7eed0f3e39ff1bb3288021dd504f7ac93e07f56d
Laolao <leenallaf98@gmail.com>
2022-07-29 20:16:31 +0300

    location fixed

--- resources/views/packages.blade.php
+++ resources/views/packages.blade.php
@@ -131,14 +131,14 @@
                                 <ul>
                                     {{-- <li><span class="flaticon-shower"></span>2</li> --}}
                                     <li><span class="flaticon-king-size"></span>3</li>
-                                    <li><span class="flaticon-location"><svg xmlns="http://www.w3.org/2000/svg"
+                                    <li><span ><svg xmlns="http://www.w3.org/2000/svg"
                                                 class="h-2 w-2" fill="none" viewBox="0 0 24 24"
                                                 stroke="currentColor" stroke-width="2" style="width:20px">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                                            </svg></span>{{ $p->country->name }}</li>
+                                            </svg></span>{{ $t->touristPlace->country->name }}</li>
                                 </ul>
                             </div>
                         </div>
